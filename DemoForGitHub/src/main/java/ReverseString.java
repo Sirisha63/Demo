0307f01bb2811b6541600477bb92c95605b1fc48
Sirisha63 <duggiralasirisha163@gmail.com>
2024-10-15 16:43:39 +0530

    Update ReverseString.java

--- DemoForGitHub/src/main/java/ReverseString.java
+++ DemoForGitHub/src/main/java/ReverseString.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the string:");
+		String str1 = sc.nextLine();
+		String str2 =" ";
+
+		for(int i=str1.length()-1;i>=0;i--){
+			str2 = str2 + str1.charAt(i);
 
 	}
+		System.out.println(str1+" Sentence Reverse is ...." +str2);
 
+}
 }
